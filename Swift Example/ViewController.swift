3a1ee604cae90c4c68fe1db52b8be9c2b1bef507
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-05-19 22:27:52 +1000

    Initial Commit

---
+++ Swift Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Example
+//
+//  Created by Jean-Pierre Gassin on 19/5/18.
+//  Copyright © 2018 Jean-Pierre Gassin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
